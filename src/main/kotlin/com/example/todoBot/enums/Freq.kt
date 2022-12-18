3eb17005a1dc553d37a52f5a73df17fb158ffe54
Antunovich Pavel <pasha.antunovich@gmail.com>
2022-12-18 15:21:12 +0300

    #2 b5 add /notify command and service that sends asynchronous notification

---
+++ src/main/kotlin/com/example/todoBot/enums/Freq.kt
@@ -0,0 +1,8 @@
+package com.example.todoBot.enums
+
+enum class Freq {
+    DAILY,
+    WEEKLY,
+    MONTHLY,
+    ONCE
+}
--- src/main/kotlin/com/example/todoBot/service/TaskService.kt
+++ src/main/kotlin/com/example/todoBot/service/TaskService.kt
@@ -1,23 +1,39 @@
 package com.example.todoBot.service
 
+import com.example.todoBot.enums.Freq
 import com.example.todoBot.enums.Status
 import com.example.todoBot.model.TaskModel
 import com.example.todoBot.repository.TasksRepository
 import com.example.todoBot.util.Utils.Companion.dateFormat
+import com.example.todoBot.util.Utils.Companion.freqFormat
+import com.example.todoBot.util.Utils.Companion.getInitialDelay
+import com.example.todoBot.util.Utils.Companion.getPeriod
+import com.example.todoBot.util.Utils.Companion.getStringFromTaskModel
 import com.example.todoBot.util.Utils.Companion.getTimeFromString
 import com.example.todoBot.util.Utils.Companion.statusFormat
 import com.example.todoBot.util.Utils.Companion.timeFormat
 import com.google.common.collect.Iterables
 import org.springframework.stereotype.Service
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.bots.AbsSender
+import java.time.LocalDate
+import java.time.LocalTime
 import java.time.ZonedDateTime
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 @Service
 class TaskService(private val tasksRepository: TasksRepository) {
+
+    private var scheduler = Executors.newSingleThreadScheduledExecutor()
 
     fun create(arguments: List<String>): String {
         val deadlineDate = arguments[arguments.lastIndex - 1]
         val deadlineTime = arguments.last()
         val name = arguments.subList(1, arguments.lastIndex - 1).joinToString(separator = " ")
+        if (name.length > 50) {
+            return "Слишком длинное название! Не стоит так подробно описывать свои задачи."
+        }
         if (!dateFormat.matches(deadlineDate)){
             return "Я не понимаю такой формат даты :P"
         }
@@ -68,13 +84,95 @@ class TaskService(private val tasksRepository: TasksRepository) {
             return sb.toString()
         }
         for (task: TaskModel in tasks) {
-            val name = task.name
-            val deadlineDate = task.deadline?.toLocalDate()
-            val deadlineTime = task.deadline?.toLocalTime()
-            val status = task.status
-            sb.append("*Название:* $name,  *срок:* $deadlineDate $deadlineTime,  *статус:* $status\n")
+            sb.append(getStringFromTaskModel(task))
         }
 
         return sb.toString()
     }
+
+    fun setNotification(chatId: Long, sender: AbsSender, args: List<String>) : String {
+        val defaultFreq = Freq.ONCE
+        val defaultDate = LocalDate.now()
+        val freq: Freq
+        val time: LocalTime
+        val date: LocalDate
+        val lastArg = args.last()
+        val secondToLast = args[args.lastIndex - 1]
+        var returnedMessage = "Неправильно вы аргументы подбираете. Попробуйте /help"
+
+
+        if (freqFormat.matches(lastArg)) {
+            freq = Freq.valueOf(lastArg)
+
+            if (timeFormat.matches(secondToLast)) {
+                time = LocalTime.parse(secondToLast)
+            } else {
+                return returnedMessage
+            }
+            date = defaultDate
+        } else {
+            freq = defaultFreq
+
+            if (timeFormat.matches(lastArg)) {
+                time = LocalTime.parse(lastArg)
+
+                date = if (dateFormat.matches(secondToLast)) LocalDate.parse(secondToLast)
+                else {
+                    return returnedMessage
+                }
+
+
+            } else {
+                return returnedMessage
+            }
+        }
+
+        val name: String = args.subList(1, args.lastIndex - 1).joinToString(" ")
+
+        if (name != "/all") {
+            val initialTask = tasksRepository.findByName(name)
+            if (initialTask == null) {
+                returnedMessage = "Не о чем уведомлять"
+                return returnedMessage
+            }
+        } else {
+            val tasks = tasksRepository.findAll()
+            if (Iterables.isEmpty(tasks)) {
+                returnedMessage = "Ой, здесь пусто UwU"
+                return returnedMessage
+            }
+        }
+
+        returnedMessage = "Уведомления включены"
+        val action = Runnable {
+            val executableMessage = StringBuilder("Какой прекрасный день! Самое время заняться тем, что давно запланировано:\n\n")
+            if (name == "/all") {
+                executableMessage.append(getAll())
+            } else {
+                val task = tasksRepository.findByName(name)
+                if (task != null) {
+                    val message = getStringFromTaskModel(task)
+                    executableMessage.append(message)
+                }
+            }
+            val sendMessage = SendMessage(chatId.toString(), executableMessage.toString())
+            sendMessage.enableMarkdown(true)
+
+            sender.execute(sendMessage)
+        }
+
+        if (scheduler.isShutdown)
+            scheduler = Executors.newSingleThreadScheduledExecutor()
+
+        val initialDelay = getInitialDelay(time, date)
+        val timeUnit = TimeUnit.SECONDS
+        val period = getPeriod(freq)
+
+        if (freq == defaultFreq) scheduler.schedule(action, initialDelay, timeUnit)
+        else scheduler.scheduleAtFixedRate(action, initialDelay, period, timeUnit)
+
+        return returnedMessage
+    }
+
+
 }
